51538a39bcca292d8fba05b2c4fbf8a8a15d24bb
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-12-19 22:59:27 +0100

    Server code

--- c-socket/socket-server.c
+++ c-socket/socket-server.c
@@ -1,51 +1,49 @@
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
  
 int main(void)
 {
-  int sockfd = 0,n = 0;
-  char recvBuff[1024];
+  int listenfd = 0,connfd = 0;
+  
   struct sockaddr_in serv_addr;
  
-  memset(recvBuff, '0' ,sizeof(recvBuff));
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
-    {
-      printf("\n Error : Could not create socket \n");
-      return 1;
-    }
+  char sendBuff[1025];  
+  int numrv;  
  
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(5000);
-  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("socket retrieve success\n");
+  
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  memset(sendBuff, '0', sizeof(sendBuff));
+      
+  serv_addr.sin_family = AF_INET;    
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+  serv_addr.sin_port = htons(5000);    
  
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
-    {
-      printf("\n Error : Connect Failed \n");
-      return 1;
-    }
+  bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
+  
+  if(listen(listenfd, 10) == -1){
+      printf("Failed to listen\n");
+      return -1;
+  }     
+  
+  while(1)
+    {      
+      connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
+  
+      strcpy(sendBuff, "Message from server");
+      write(connfd, sendBuff, strlen(sendBuff));
  
-  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
-      recvBuff[n] = 0;
-      if(fputs(recvBuff, stdout) == EOF)
-    {
-      printf("\n Error : Fputs error");
-    }
-      printf("\n");
-    }
- 
-  if( n < 0)
-    {
-      printf("\n Read Error \n");
-    }
+      close(connfd);    
+      sleep(1);
+    } 
  
   return 0;
 }
